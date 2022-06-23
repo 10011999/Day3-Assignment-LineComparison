50c87d2e39d6729b3042ab0270722e7ebcd7a824
10011999 <motewaraditya77@gmail.com>
2022-06-23 20:39:14 +0530

    Added: Day-3-Assignment

---
+++ LineComparisonProblem.java
@@ -0,0 +1,37 @@
+package DayThreeAssignment;
+
+import java.util.Scanner;
+
+public class LineComparisonProblem {
+    Scanner sc = new Scanner(System.in);
+    public float insertCoOrdinates()
+    {
+        System.out.println("Enter value of x1:");
+        int x1 = sc.nextInt();
+        System.out.println("Enter value of x2:");
+        int x2 = sc.nextInt();
+        System.out.println("Enter value of y1:");
+        int y1 = sc.nextInt();
+        System.out.println("Enter value of y2:");
+        int y2 = sc.nextInt();
+
+        return (float)(Math.sqrt((x2-x1)*(x2-x1)+((y2-y1)*(y2-y1))));
+    }
+    public static void main(String[] args)
+    {
+
+        System.out.println("Welcome to line comparison computation program");
+        LineComparisonProblem line1 = new LineComparisonProblem();
+        LineComparisonProblem line2 = new LineComparisonProblem();
+
+        if(line1.insertCoOrdinates() > line2.insertCoOrdinates())
+        {
+            System.out.println("line1 is greater ....");
+        }
+        else
+        {
+            System.out.println("line 2 is greater");
+        }
+
+    }
+}
